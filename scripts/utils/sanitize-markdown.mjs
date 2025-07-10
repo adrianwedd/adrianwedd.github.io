ed0fbe05b4ab13ec6bfd41eb4227591a074ee74c
Adrian Wedd <adrian@adrianwedd.com>
2025-07-11 00:16:11 +1000

    feat(scripts): sanitize markdown output

---
+++ scripts/utils/sanitize-markdown.mjs
@@ -0,0 +1,10 @@
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+export function sanitizeMarkdown(text) {
+  return DOMPurify.sanitize(text);
+}
+
