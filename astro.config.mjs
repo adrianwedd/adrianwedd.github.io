5ad9fc1609dcb2f6c37c93aa313273ded10b1a0d
Adrian Wedd <adrian@adrianwedd.com>
2025-07-13 11:22:21 +1000

    feat(config): secure external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,16 @@
+import rehypeExternalLinks from 'rehype-external-links';
+
 export default {
   site: 'https://github.adrianwedd.com',
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer', 'nofollow'],
+        },
+      ],
+    ],
+  },
 };
